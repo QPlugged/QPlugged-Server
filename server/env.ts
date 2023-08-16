907445a14be3c8eeeabd43c2779a6d1d179b3e1c
Flysoft <flysoftbeta@outlook.com>
2023-08-16 11:54:52 +0800

    chore: 修改环境变量名称

--- server/env.ts
+++ server/env.ts
@@ -6,9 +6,10 @@ export const resourceDir: string = global.__QP_DIR;
 export const isProduction: boolean = IS_PROD;
 
 export const isInspectorMode =
-    (process.env.QP_INSPECTOR !== undefined &&
-        !!parseInt(process.env.QP_INSPECTOR)) ||
+    (process.env.QP_SERVER_INSPECTOR !== undefined &&
+        !!parseInt(process.env.QP_SERVER_INSPECTOR)) ||
     false;
 export const listenPort =
-    (process.env.QP_PORT !== undefined && parseInt(process.env.QP_PORT)) ||
+    (process.env.QP_SERVER_PORT !== undefined &&
+        parseInt(process.env.QP_SERVER_PORT)) ||
     (isProduction ? findFreePort() : 15321);
